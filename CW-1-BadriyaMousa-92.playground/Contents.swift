1acc41552aec5342053a7e680c6ae89a95b7bfc4
92-BadriyaMousaAlajmi <Badora2520@gmail.com>
2020-06-13 20:46:46 +0300

    done

---
+++ CW-1-BadriyaMousa-92.playground/Contents.swift
@@ -0,0 +1,65 @@
+import UIKit
+
+//Part1
+
+let Name = "Badriya"
+
+var age = 15
+
+var height = 1.64
+
+var hoppy1 = "PlayingThePiano"
+
+var hoppy2 = "RidingHorse"
+
+print("MY Name is \(Name), I am \(age) year old, my hoppies are \(hoppy1) and \(hoppy2)" )
+
+//Part2
+
+let FriendName = "منيرة"
+
+var FriendAge = 16
+
+var FriendHeight = 1.59
+
+var FriendHoppy1 = "PlayingFlute"
+
+var FriendHoppy2 = "swimming"
+
+var age_difference = age - FriendAge
+
+// age
+age == FriendAge
+age != FriendAge
+age > FriendAge
+age < FriendAge
+age >= FriendAge
+age <= FriendAge
+
+// height
+height == FriendHeight
+height <= FriendHeight
+height >= FriendHeight
+height < FriendHeight
+height  > FriendHeight
+height != FriendHeight
+
+// hoppy
+hoppy1 == FriendHoppy1
+hoppy1 == FriendHoppy2
+hoppy1 != FriendHoppy1
+hoppy1 != FriendHoppy2
+hoppy1 <= FriendHoppy1
+hoppy1 >= FriendHoppy2
+hoppy1 < FriendHoppy1
+hoppy1 < FriendHoppy2
+hoppy1 > FriendHoppy1
+hoppy1 > FriendHoppy2
+
+// Name
+Name == FriendName
+Name != FriendName
+Name <= FriendName
+Name >= FriendName
+Name < FriendName
+Name > FriendName
